9547469925004df952df99f7f660fe818af671d4
CARLOS <carlosayma22@gmail.com>
2019-05-12 00:09:14 -0500

    CALCULADORA

--- app/src/main/java/com/example/calcupro/MainActivity.java
+++ app/src/main/java/com/example/calcupro/MainActivity.java
@@ -2,37 +2,66 @@ package com.example.calcupro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    EditText n1 , n2;
-    Button btnsumar,btnrestar;
-    TextView r;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et1, et2;
+    Button btn1, btn2;
+    TextView tv1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        n1=(EditText)findViewById(R.id.et1);
-        n2=(EditText)findViewById(R.id.et2);
-        r=(TextView)findViewById(R.id.resultado);
-        btnsumar=(Button)findViewById(R.id.btn1);
-        btnrestar=(Button)findViewById(R.id.btn2);
-        btnsumar.setOnClickListener(this);
-        btnrestar.setOnClickListener(this);
+
+        et1 = (EditText) findViewById(R.id.txt_num1);
+        et2 = (EditText) findViewById(R.id.txt_num2);
+        tv1 = (TextView) findViewById(R.id.txt_resultado);
+        btn1 = (Button) findViewById(R.id.btnSumar);
+        btn2 = (Button) findViewById(R.id.btnRestar);
+
 
     }
-    @Override
-    public void onClick(View v){
-        int valor1 = Integer.parseInt(n1.getText().toString());
-        int valor2 = Integer.parseInt(n2.getText().toString());
-        int s = valor1+valor2;
-        r.setText(s+"");
-        int res = valor1-valor2;
-        r.setText(res+"");
+
+    //ESTE METODO REALIZA LA SUMA
+    public void Sumar(View view) {
+        String valor1 = et1.getText().toString();
+        String valor2 = et2.getText().toString();
+
+        int num1 = Integer.parseInt(valor1);
+        int num2 = Integer.parseInt(valor2);
+
+        int suma = num1 + num2;
+
+
+        String result = String.valueOf(suma);
+        tv1.setText(result);
+
+
     }
+
+    public void Restar(View view) {
+        String valor1 = et1.getText().toString();
+        String valor2 = et2.getText().toString();
+
+        int num1 = Integer.parseInt(valor1);
+        int num2 = Integer.parseInt(valor2);
+
+        int resta = num1 - num2;
+
+
+        String result = String.valueOf(resta);
+        tv1.setText(result);
+
+
+    }
+
 
 }
